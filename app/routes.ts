d532fc520123dc5dc83348f6e8bc986587fb3db1
Thomas Esseul <thomas@follow.fr>
2025-02-18 15:29:47 +0100

    feat(home): remove wip route and welcome page

--- app/routes.ts
+++ app/routes.ts
@@ -1,13 +1,5 @@
-import {
-	type RouteConfig,
-	index,
-	layout,
-	route,
-} from '@react-router/dev/routes'
+import { type RouteConfig, index, layout } from '@react-router/dev/routes'
 
 export default [
-	layout('./layouts/DefaultLayout/index.ts', [
-		index('routes/home.tsx'),
-		route('wip', 'routes/resume.tsx'),
-	]),
+	layout('./layouts/DefaultLayout/index.ts', [index('routes/resume.tsx')]),
 ] satisfies RouteConfig
