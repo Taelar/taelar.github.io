bea926b9ba885cd31c74613b6cf2462e821452d2
Thomas Esseul <thomas@follow.fr>
2025-02-25 09:18:09 +0100

    chore(rr): add SSG config

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,4 +3,8 @@ import type { Config } from '@react-router/dev/config'
 export default {
 	ssr: false,
 	buildDirectory: 'dist',
+	// Ajouter ici les routes qui doivent générer statiquement au moment du build
+	async prerender() {
+		return ['/']
+	},
 } satisfies Config
