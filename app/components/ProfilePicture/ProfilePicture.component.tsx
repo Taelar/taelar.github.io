f5fb824a838c682a8120e7821f8e3c5f026e475c
Thomas Esseul <thomas@follow.fr>
2025-05-20 15:24:27 +0200

    feat(profilePicture): add easter egg

--- app/components/ProfilePicture/ProfilePicture.component.tsx
+++ app/components/ProfilePicture/ProfilePicture.component.tsx
@@ -1,13 +1,19 @@
 import type { FC } from 'react'
 import styles from './ProfilePicture.module.scss'
-// import RaccoonProfilePicture from './assets/profile_raccoon.jpg'
+import RaccoonProfilePicture from './assets/profile_raccoon.jpg'
 import Picture from './assets/profile.jpg'
 import Badge from './assets/badge.svg'
+import classNames from 'classnames'
 
 export const ProfilePicture: FC = () => {
 	return (
 		<div className={styles['container']}>
 			<img className={styles['picture']} src={Picture} alt="Thomas Esseul" />
+			<img
+				className={classNames(styles['picture'], styles['easterEgg'])}
+				src={RaccoonProfilePicture}
+				alt="Un raton-laveur"
+			/>
 			<img className={styles['badge']} src={Badge} alt="" />
 		</div>
 	)
